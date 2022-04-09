a5b5f3276fcda40a72bacd5b030250121feba123
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-04-09 14:04:11 +0530

    [Vivek] UC3 Append Element Completed

--- DSLinkedList/ModifiedLinkedList.cs
+++ DSLinkedList/ModifiedLinkedList.cs
@@ -9,7 +9,7 @@ namespace DSLinkedList
             if (head == null)
             {
                 head = nextNode;      // If head is null then hode will be new node
-                Console.WriteLine($"{newNode.data} is inserted at first of linked list");
+                Console.WriteLine($"{nextNode.data} is inserted at first of linked list");
                 return;
             }
             nextNode.next = this.head;      // newnode pointing to where head is currently pointing
@@ -31,6 +31,27 @@ namespace DSLinkedList
                     Console.WriteLine(temp.data + " ");
                     temp = temp.next;       // Go to next node
                 }
+            }
+        }
+
+        public void Append(T data) // creating generic method to Add element at first of linked list
+        {
+            Node<T> newNode = new Node<T>(data); // Creating a new node 
+            if (head == null)
+            {
+                head=newNode; // If head pointing to null then hode is directly pointing to new node
+                Console.WriteLine($"{newNode.data} is appended in linked list");
+                return;
+            }
+            else
+            {
+                Node<T> temp = this.head; // taking head as temp node
+                while (temp.next != null) // Find a last node 
+                {
+                    temp = temp.next;// Go to next node till last nast node               
+                }
+                temp.next = newNode; // Add new Node at last
+                Console.WriteLine($"{newNode.data} is appended in linked list");
             }
         }
     }
--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -8,16 +8,12 @@ namespace DSLinkedList
         static void Main(string[] args)
         {
             int i=1;
-            
-            LinkedList<int> list = new LinkedList<int>();       // Created new Linked List 
-            list.AddFirst(56);      // Adding first element in list
-            list.AddLast(30);       // Adding element at last position
-            list.AddLast(70);       // Again adding one element after 30
-            foreach (var item in list)
-            {
-                Console.WriteLine($"Element at {i} position {item}"); 
-                i++;               
-            }
+            ModifiedLinkedList<int> list1 = new ModifiedLinkedList<int>();
+            //LinkedList<int> list = new LinkedList<int>();       // Created new Linked List 
+            list1.Append(56);      // Adding first element in list
+            list1.Append(30);       // Adding element at last position
+            list1.Append(70);       // Again adding one element after 30
+            list1.DisplayElement();
         }
     }
 }
