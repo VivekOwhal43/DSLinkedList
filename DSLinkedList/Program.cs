7800e40fc98bec7800c273e2171c4e5b54b11de5
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-04-15 12:17:23 +0530

    [Vivek] Added Metod to Insert Element Efter A Perticular Element

--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -9,11 +9,13 @@ namespace DSLinkedList
         {
             ModifiedLinkedList<int> list = new ModifiedLinkedList<int>();
             //LinkedList<int> list = new LinkedList<int>();       // Created new Linked List 
-            list.Append(56); // Calling method of CustomLinkedList generic class to append element
-            list.Append(30); 
-            list.Append(70);  
-            list.DisplayElement(); // Calling method of ModifiedLinkedList generic class to display linkedlist element
-            list.Search(30); // Calling method of ModifiedLinkedList generic class to search element in linkedlist
+            list.Append(56);                // Calling method of ModifiedLinkedList generic class to append emelents
+            list.Append(30);                
+            list.Append(70);                
+            list.DisplayElement();          // Calling method of ModifiedLinkedList generic class to display linkedlist element
+            list.Search(30);                // Calling method of ModifiedLinkedList generic class to search element in linkedlist
+            list.InsertElement(30, 40);
+            list.DisplayElement();          // Output must be 56,30,70,40
         }
     }
 }
